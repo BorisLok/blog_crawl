dd2735237c6a7f6eaa870b994e172b6c86f831bf
boris <boris@velodash.co>
2022-02-10 15:06:04 +0800

    temp

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import io
+from datetime import datetime, timezone
 
 import yaml
+from dotenv import load_dotenv, find_dotenv
+
 from core import crawl_from_website, get_from_database
 from magazine import Magazine, MagazineBuilder
 
 if __name__ == '__main__':
-    magazine = MagazineBuilder()
-    magazine.set_id("123")
-    magazine.set_url("https://blog.velodash.co/2022/01/26/2022-spring-couplets/")
-    crawl_from_website(magazine.magazine)
-    print(magazine.magazine.to_json())
+    load_dotenv(find_dotenv(), verbose=True)
+    time = datetime(2020, 1, 1, 0, 0, 0).replace(tzinfo=timezone.utc).timestamp()
+    magazines = get_from_database(time)
